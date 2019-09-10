3d4df7f69e7e0b768219bada96149d436683b2d9
Christopher O'Bannon <Crobwan@gmail.com>
2019-09-10 11:45:44 -0500

    SQL code added to database file

--- database.sql
+++ database.sql
@@ -7,4 +7,47 @@ CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+    "access_level" INT DEFAULT = 0 NOT NULL
+);
+
+CREATE TABLE "user"
+(
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "user_daily_entry"
+(
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "date" DATE
+);
+
+CREATE TABLE "habit_prompts"
+(
+    "id" SERIAL PRIMARY KEY,
+    "habit_prompt_text" VARCHAR(350)
+);
+
+CREATE TABLE "user_response_habit"
+(
+    "id" SERIAL PRIMARY KEY,
+    "user_response" BOOLEAN,
+    "habbit_id" INT REFERENCES "habit_prompts",
+    "daily_entry_id" INT REFERENCES "user_daily_entry"
+);
+
+CREATE TABLE "self_report_prompts"
+(
+    "id" SERIAL PRIMARY KEY,
+    "self_report_prompt_text" VARCHAR(350)
+);
+
+CREATE TABLE "user_response_self_report"
+(
+    "id" SERIAL PRIMARY KEY,
+    "user_response" BOOLEAN,
+    "self_report_id" INT REFERENCES "self_report_prompts",
+    "user_entry_id" INT REFERENCES "user_daily_entry"
 );
