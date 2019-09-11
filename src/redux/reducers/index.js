4b02032cda86eb1e6c8ef849d4b4de0b6f06e8a7
Christopher O'Bannon <Crobwan@gmail.com>
2019-09-11 13:35:56 -0500

    creates a self report responseses reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,6 +4,7 @@ import loginMode from './loginModeReducer';
 import user from './userReducer';
 import habitPrompt from './habitPromptReducer';
 import selfReportPrompt from './selfReportPrompt';
+import selfReportResponses from './selfReportResponses';
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -16,6 +17,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   habitPrompt,
   selfReportPrompt,
+  selfReportResponses,
 });
 
 export default rootReducer;
