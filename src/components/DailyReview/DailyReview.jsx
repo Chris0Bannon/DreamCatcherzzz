bb3c48047b4fb93483de8497e332577e64624e0b
Christopher O'Bannon <Crobwan@gmail.com>
2019-09-12 14:04:05 -0500

    daily revew will now display the users entry and the matching prompts on the DOM, styling needed

--- src/components/DailyReview/DailyReview.jsx
+++ src/components/DailyReview/DailyReview.jsx
@@ -1,26 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Button from '@material-ui/core/Button'
+import Button from "@material-ui/core/Button";
 class DailyEntryReview extends Component {
   render() {
     return (
       <div>
         <h1>Hello From Daily Review</h1>
-        <table>
-            <thead>
-            </thead>
-            <tbody>
-                <tr>{this.props.reduxStore.habitResponses.map(item => (
-                    <td>
-                        ANSWER: {item}
-                    </td>
-                ))}
+        <table className="bestTable">
+          <thead></thead>
+          <tbody>
+            <tr>
+              {this.props.reduxStore.allHabitPrompts.map(item => (
+                <td key={item.id}>{item.habit_prompt_text}</td>
+              ))}
             </tr>
-            </tbody>
+            <tr>
+              {this.props.reduxStore.habitResponses.map(item => (
+                <td>ANSWER: {item}</td>
+              ))}
+            </tr>
+            <tr>
+              {this.props.reduxStore.allSelfReportPrompts.map(item => (
+                <td className="sure" key={item.id}>
+                  {item.habit_prompt_text}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              {this.props.reduxStore.selfReportResponses.map(item => (
+                <td className="sure">ANSWER: {item}</td>
+              ))}
+            </tr>
+          </tbody>
         </table>
-        <p>{JSON.stringify(this.props.reduxStore.habitResponses)}</p>
-        <p>{JSON.stringify(this.props.reduxStore.selfReportResponses)}</p>
-        <Button variant = "contained" color = "primary" >Submit</Button>
+
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
       </div>
     );
   }
