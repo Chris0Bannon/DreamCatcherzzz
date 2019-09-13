9b7968f808614fdbd8eec81315a7e1f7273a5807
Christopher O'Bannon <Crobwan@gmail.com>
2019-09-12 20:21:17 -0500

    habbit router almost completes async POST route

--- server/routes/habitPrompts.router.js
+++ server/routes/habitPrompts.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pool = require('../modules/pool');
-
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const router = express.Router();
 
 router.get(`/:id`, (req,res) => {
@@ -18,6 +18,8 @@ router.get(`/:id`, (req,res) => {
 
 router.get(`/`, (req, res) => {
     let queryText = `SELECT * FROM "habit_prompts";`;
+    console.log('req.user GET:', req.user);
+    
     pool.query(queryText)
         .then((result) => {
             console.log(result);
@@ -28,9 +30,11 @@ router.get(`/`, (req, res) => {
         });
 });
 
-router.post(`/`, async (req, res) => {
+router.post(`/`, rejectUnauthenticated, async (req, res) => {
     console.log('logging req.body in habitRouter', req.body);
-    
+   console.log('req.user:', req.user);
+   
+
     const user = req.user.id;
     const connection =await pool.connect()
     try{
@@ -40,12 +44,22 @@ router.post(`/`, async (req, res) => {
 	RETURNING "id";`;
     const result = await connection.query( sqlAddUserEntry, [user])
     const entryId =result.rows[0].id
-        const sqlAddUserResponse = `INSERT INTO "user_response_self_report" ("user_response", "self_report_id", "user_entry_id")
-    VALUES ($1, $2, $3);`
+        const sqlAddUserResponse = `INSERT INTO "user_response_habit" ("user_response", "habit_id", "daily_entry_id")
+    VALUES ($1, $2, $3);`;
+    await connection.query(sqlAddUserResponse, ['true', 3, entryId])
     
+    // for (let i =0; i < req.body.user_response; i++){
+    //     console.log('I am actually looping', item[i]);
+    //     await connection.query(sqlAddUserResponse, [item, item[i], entryId])
+    // }
+   await connection.query('COMMIT');
+   res.sendStatus(201);
 }catch(error){
-    console.log(error);
-    
+    await connection.query('ROLLBACK');
+    console.log('error in server side habit response POST', error);
+    res.sendStatus(500)
+}finally{
+    connection.release()
 }
 })
 
