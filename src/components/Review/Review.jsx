2f38819aa583d2ffe0f28f7c372763217c4c0959
Christopher O'Bannon <Crobwan@gmail.com>
2019-09-14 17:26:46 -0500

    creates a router for user entries, declares action type for user entry reducer

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -3,11 +3,19 @@ import Button from '@material-ui/core/Button';
 
 class Review extends Component {
 
+fetchUserEntries =(event) => {
+  console.log('you clicked a fetcher');
+  let action = {
+    type: 'FETCH_ALL_USER_ENTRIES'
+  }
+}
+
     render() {
         return (
           <div>
             <h1>Hello From Review</h1>
-            <Button onClick = {() => {this.props.history.push('/home')}} className = "App">Back</Button>
+            <Button onClick = {() => {this.props.history.push('/home')}} className = "tryItBtn">Back</Button>
+            <Button style = {{background: 'radial-gradient(circle, rgba(50,210,232,0) 4%, rgba(9,224,254,1) 65%, rgba(45,24,78,1) 96%)'}} onClick = {this.fetchUserEntries}>Get anything</Button>
           </div>
         );
     }
